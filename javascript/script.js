243fd566e82646043e143dd6bdc545ee06980259
Veliko <Kardziev>
2023-05-19 10:14:59 +0200

    added javascript-functionality

--- javascript/script.js
+++ javascript/script.js
@@ -4,5 +4,10 @@ const answer = document.querySelectorAll('[data-js="answerBlock"]');
 showAnswerButton.forEach((answerButton, index) => {
 	answerButton.addEventListener("click", () => {
 		answer[index].classList.toggle("answer--hidden");
+		if (answerButton.textContent === "Hide Answer") {
+			answerButton.textContent = "Show Answer";
+		} else {
+			answerButton.textContent = "Hide Answer";
+		}
 	});
 });
