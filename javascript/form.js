21ab6eaeb48a60189063323a538f275ac509b3a8
Veliko <Kardziev>
2023-05-26 15:22:54 +0200

    smaller adjustments

--- javascript/form.js
+++ javascript/form.js
@@ -7,18 +7,19 @@ form.addEventListener("submit", (event) => {
 	const data = Object.fromEntries(formData);
 
 	const questioncard = document.createElement("section");
-	const bookmark = document.createElement("img");
+	const bookmark = document.createElement("button");
+	const bookmarkIcon = document.createElement("img");
 	const questioncardHeader = document.createElement("h2");
 	const answer = document.createElement("p");
 	const tagList = document.createElement("ul");
 
 	questioncard.classList.add("questioncard", "questioncard--new-card");
-	bookmark.classList.add("bookmark");
+	bookmark.classList.add("bookmark", "button--bookmark");
+	bookmark.setAttribute("data-js", "bookmarkIcon");
 	questioncardHeader.classList.add("questioncard__header");
 	answer.classList.add("answer");
 	tagList.classList.add("taglist");
-	bookmark.setAttribute("src", "./assets/bookmark.svg");
-	bookmark.setAttribute("data-js", "bookmarkIcon");
+	bookmarkIcon.setAttribute("src", "./assets/bookmark.svg");
 
 	questioncardHeader.textContent = data.question;
 	answer.textContent = data.answer;
@@ -26,6 +27,7 @@ form.addEventListener("submit", (event) => {
 	main.append(questioncard);
 	questioncard.style.marginTop = "50px";
 	questioncard.append(bookmark);
+	bookmark.append(bookmarkIcon);
 	questioncard.append(questioncardHeader);
 	questioncard.append(answer);
 	questioncard.append(tagList);
