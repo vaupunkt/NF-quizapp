7b1d067e62aafbaf6c12a456932221ea05b39cc4
Veliko <Kardziev>
2023-05-26 12:16:36 +0200

    add form to navigation and implement adding new Cards as a feature

---
+++ javascript/form.js
@@ -0,0 +1,60 @@
+const form = document.querySelector('[data-js="questionForm"]');
+const main = document.querySelector('[data-js="main"]');
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const formData = new FormData(event.target);
+	const data = Object.fromEntries(formData);
+	console.log(data);
+
+	const questioncard = document.createElement("section");
+	const bookmark = document.createElement("img");
+	const questioncardHeader = document.createElement("h2");
+	const answer = document.createElement("p");
+	const tagList = document.createElement("ul");
+
+	questioncard.classList.add("questioncard", "questioncard--newCard");
+	bookmark.classList.add("bookmark");
+	questioncardHeader.classList.add("questioncard__header");
+	answer.classList.add("answer");
+	tagList.classList.add("taglist");
+    bookmark.setAttribute("src", "./assets/bookmark.svg");
+    bookmark.setAttribute("data-js", "bookmarkIcon")
+
+	questioncardHeader.textContent = data.question;
+	answer.textContent = data.answer;
+
+	main.append(questioncard);
+	questioncard.style.marginTop = "50px";
+	questioncard.append(bookmark);
+	questioncard.append(questioncardHeader);
+	questioncard.append(answer);
+	questioncard.append(tagList);
+
+	let tagArray = data.tags.split(",");
+	for (let i = 0; i < tagArray.length; i++) {
+		const tag = document.createElement("li");
+		tag.classList.add("tag");
+		tagArray[i] = "#" + tagArray[i].trim();
+		tag.textContent = tagArray[i];
+		tagList.append(tag);
+	}
+
+	form.reset();
+	form.question.focus();
+});
+
+const questionFormTextareas = document.querySelectorAll(
+	'[data-js="questionFormTextarea"]'
+);
+
+questionFormTextareas.forEach((textarea, index) => {
+	let counter = 150;
+	const lettercounter = document.querySelectorAll('[data-js="lettercounter"]');
+	textarea.addEventListener("input", () => {
+		if (counter > 0) {
+			counter--;
+			lettercounter[index].textContent = counter + " Characters left";
+		}
+	});
+});
